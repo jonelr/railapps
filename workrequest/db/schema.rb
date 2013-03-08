67c79b74ee697d78fd52c892feb17619b09a4aaf
Joe R <joe.net+github@me.com>
2013-03-07 20:52:44 -0600

    Devise added

--- workrequest/db/schema.rb
+++ workrequest/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130308023855) do
+ActiveRecord::Schema.define(:version => 20130308024630) do
 
   create_table "areas", :force => true do |t|
     t.string   "title"
@@ -63,6 +63,24 @@ ActiveRecord::Schema.define(:version => 20130308023855) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
   create_table "work_request_comments", :force => true do |t|
     t.string   "comment"
